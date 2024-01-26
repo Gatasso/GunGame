8df1f365efa7fbf1a5f63101040084bdce5f964f
Galasso <galassoworkstation@gmail.com>
2024-01-25 23:14:27 -0300

    Define the Player Entitie

---
+++ Application/src/java/dev/galasso/matheus/core/entities/Player/Player.java
@@ -0,0 +1,8 @@
+package java.dev.galasso.matheus.core.entities.Player;
+
+public class Player {
+    private int idOnServer;
+    private final int HEALTH_INITIAL = 100;
+    private Budget budget;
+
+}
