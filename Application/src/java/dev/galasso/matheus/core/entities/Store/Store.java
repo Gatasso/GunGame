13f4e4c1031b70061f00256131d71f30f80b134a
Galasso <galassoworkstation@gmail.com>
2024-01-25 23:19:39 -0300

    Define the Store Entity

---
+++ Application/src/java/dev/galasso/matheus/core/entities/Store/Store.java
@@ -0,0 +1,8 @@
+package java.dev.galasso.matheus.core.entities.Store;
+
+import java.dev.galasso.matheus.core.entities.Weapon.TypeWeapon;
+
+public class Store {
+    private TypeWeapon availableWeapons;
+
+}
