61200cea928ec1fb677bc193633df1f67cbfb19d
Galasso <galassoworkstation@gmail.com>
2024-01-30 12:52:03 -0300

    Implement WeaponAction methods to the Weapon Abstract Class

--- Application/src/java/dev/galasso/matheus/core/entities/Weapon/domain/Weapon.java
+++ Application/src/java/dev/galasso/matheus/core/entities/Weapon/domain/Weapon.java
@@ -3,28 +3,32 @@ package java.dev.galasso.matheus.core.entities.Weapon.domain;
 import java.dev.galasso.matheus.core.entities.Weapon.interfaces.WeaponActions;
 
 public abstract class Weapon implements WeaponActions {
-    private String name;
-    protected double basePrice, baseDamage, maxAmmo, blltsOnClip;
+    protected double basePrice, baseDamage, ammo, blltsOnClip;
+    private final int MAX_BLLT_ON_CLIP;
 
     public Weapon(int basePrice, int baseDamage, int maxAmmo, int blltsOnClip) {
         this.basePrice = basePrice;
         this.baseDamage = baseDamage;
-        this.maxAmmo = maxAmmo;
-        this.blltsOnClip = blltsOnClip;
+        this.ammo = maxAmmo;
+        this.blltsOnClip = this.MAX_BLLT_ON_CLIP = blltsOnClip;
     }
 
     @Override
     public void shoot() {
-
+        while (/*player.clicks() &&*/ blltsOnClip > 0) {
+            blltsOnClip--;
+        }
+        reload();
     }
 
     @Override
     public void reload() {
-
-    }
-
-    @Override
-    public boolean aim() {
-        return false;
+        if (this.ammo < this.MAX_BLLT_ON_CLIP) {
+            blltsOnClip = this.ammo;
+            this.ammo = 0;
+        } else {
+            blltsOnClip = this.MAX_BLLT_ON_CLIP;
+            this.ammo = this.ammo - this.MAX_BLLT_ON_CLIP;
+        }
     }
 }
